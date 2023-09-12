b792ac4790d729260571814f5532f7848e53103e
niosni <niklas.niemela@hotmail.com>
2023-09-12 23:43:19 +0300

    Puhelinluettelot clean up code

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
@@ -33,65 +32,64 @@ const App = () => {
 
   const updateNumber = () => {
     const oldPersonObject = persons.find(p => p.name == newName)
-    const newPersonObject = {
+    const updatedPersonObject = {
       ...oldPersonObject,
       number: newNumber
     }
     personService
-      .update(oldPersonObject.id, newPersonObject)
+      .update(oldPersonObject.id, updatedPersonObject)
       .then(returnedPerson => 
         setPersons(persons.map(person =>
-          person.id !== newPersonObject.id ? person : returnedPerson
+          person.id !== updatedPersonObject.id ? person : returnedPerson
         ))
       )
   }
 
-  const addDetails = (event) => {
-    event.preventDefault()
-    
+  const askToUpdateNumber = () => {
     //handle duplicate names
     const allNames = persons.map(person => person.name)
     if (allNames.includes(newName)) {
       if (confirm(`${newName} is already added to the phonebook. Replace the old number with a new one?`)) {
         updateNumber()
-        return
+        return true
       } else {
-        return
+        return false
       }
     }
+  }
 
-    let personObject = {
-      name: newName,
-    }
-    //Add number if one given
-    if (newNumber) {
-      personObject = {
-        ...personObject,
-        number: newNumber,
+  const addDetails = (event) => {
+    event.preventDefault()
+    // Check if the name already exists
+    const personWasUpdated = askToUpdateNumber()
+    if (!personWasUpdated) {
+      const newPersonObject = {
+        name: newName,
+        number: newNumber
       }
-      setNewNumber('')
+  
+      personService
+        .create(newPersonObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
     }
-
-    personService
-      .create(personObject)
-      .then(returnedPerson => {
-        setPersons(persons.concat(returnedPerson))
-        setNewName('')
-      })
   }
 
   const removePerson = (id, name) => {
     if (confirm(`Delete ${name}?`)) {
       personService
-      .remove(id)
-      .then(response => {
-        setPersons(persons.filter( person =>
-          person.id !== id
-        ))
-      })
-      .catch(error => {
-        console.log('moro');
-      })
+        .remove(id)
+        .then(response => {
+          setPersons(persons.filter( person =>
+            person.id !== id
+          ))
+        })
+        .catch(error => {
+          alert('The person you tried to remove is not on the database. Please refresh the page.')
+        })
     }
     return
   }
