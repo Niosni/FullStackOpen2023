15dc51a26f959b9e0ecbd88ce43ed933085409cb
niosni <niklas.niemela@hotmail.com>
2023-10-17 23:06:17 +0300

    Task 4.5 favorite blog test

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -13,7 +13,33 @@ const totalLikes = (blogs) => {
   return totalLikes
 }
 
+const favoriteBlog = (blogs) => {
+  if (Object.keys(blogs).length === 0) {
+    return null
+  }
+  let newBlog = {
+    title: blogs[0].title,
+    author: blogs[0].author,
+    likes: blogs[0].likes
+  }
+  if (blogs.length === 1){
+    return newBlog
+  } else {
+    blogs.forEach(blog => {
+      if(blog.likes > newBlog.likes) {
+        newBlog = {
+          title: blog.title,
+          author: blog.author,
+          likes: blog.likes
+        }
+      }
+    })
+    return newBlog
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
