246ea3d5f9b11c23a812638ee03248261a1c40e5
niosni <niklas.niemela@hotmail.com>
2023-10-30 19:09:52 +0200

    Add Noteapp frontend login and logout functionality

--- osa2/part2/src/App.jsx
+++ osa2/part2/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import noteService from './services/notes'
+import loginService from './services/login'
 import './index.css'
 
 const App = () => {
@@ -9,7 +10,9 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
-
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -19,6 +22,14 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
   if (!notes) {
     return null
   }
@@ -77,6 +88,74 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+
+      noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form>  
+  )
+
+  const logoutUser = () => {
+    window.localStorage.removeItem('loggedNoteappUser')
+    setUser(null)
+    setErrorMessage(
+      `${user.name} logged out.`
+    )
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
+
   const notesToShow = showAll
   ? notes
   : notes.filter(note => note.important)
@@ -84,28 +163,32 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+
       <Notification message={errorMessage} />
+
+      {!user && loginForm()}
+      {user && <div>
+       <p>{user.name} logged in <button onClick={logoutUser}>Logout</button></p>
+       
+         {noteForm()}
+      </div>
+      }
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? 'important' : 'all'}
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => 
+        {notesToShow.map((note, i) => 
           <Note
-            key={note.id}
-            note={note}
+            key={i}
+            note={note} 
             toggleImportance={() => toggleImportanceOf(note.id)}
           />
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input 
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>
+
       <Footer />
     </div>
   )
--- osa2/part2/src/services/notes.js
+++ osa2/part2/src/services/notes.js
@@ -1,6 +1,12 @@
 import axios from 'axios'
 const baseUrl = '/api/notes'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = () => {
     const request = axios.get(baseUrl)
     // const nonExisting = {
@@ -12,13 +18,14 @@ const getAll = () => {
     return request.then(response =>  response.data)
   }
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => {
-    console.log(response.data);
-    return response.data
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token },
   }
-    )
+
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+
 }
 
 const update = (id, newObject) => {
@@ -27,4 +34,4 @@ const update = (id, newObject) => {
 }
 
 
-export default { getAll, create, update }
+export default { getAll, create, update, setToken }
