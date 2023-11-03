01aa66735e76fbf154cc2715b9c0816485d67d68
niosni <niklas.niemela@hotmail.com>
2023-11-03 12:55:37 +0200

    Notes app eslint and proptypes

--- osa2/part2/src/components/LoginForm.jsx
+++ osa2/part2/src/components/LoginForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({ login }) => {
   const [username, setUsername] = useState('')
@@ -39,6 +40,10 @@ const LoginForm = ({ login }) => {
       </form>
     </div>
   )
+}
+
+LoginForm.propTypes = {
+  login: PropTypes.func.isRequired,
 }
 
 export default LoginForm
